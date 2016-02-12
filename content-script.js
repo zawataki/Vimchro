cbb1749e2901d2f8f6fa5b15657d95498ea4a750
Yuki Takizawa <y.takizawa64@gmail.com>
2016-02-12 14:04:32 +0900

    Add insert mode.

--- content-script.js
+++ content-script.js
@@ -1,20 +1,31 @@
 $(function(){
+
+  var isInsertMode = false;
+
+  $("input").focus(function(){
+    isInsertMode = true;
+  }).blur(function(){
+    isInsertMode = false;
+  });
+
 	$(window).keydown(function(e){
     var n = 10;
 
-    switch ( String.fromCharCode(e.keyCode) ) {
-      case 'H':
-        window.scrollBy( n * -1, 0 );
-        break;
-      case 'J':
-        window.scrollBy( 0, n );
-        break;
-      case 'K':
-        window.scrollBy( 0, n * -1 );
-        break;
-      case 'L':
-        window.scrollBy( n, 0 );
-        break;
+    if ( !isInsertMode ) {
+      switch ( String.fromCharCode(e.keyCode) ) {
+        case 'H':
+          window.scrollBy( n * -1, 0 );
+          break;
+        case 'J':
+          window.scrollBy( 0, n );
+          break;
+        case 'K':
+          window.scrollBy( 0, n * -1 );
+          break;
+        case 'L':
+          window.scrollBy( n, 0 );
+          break;
+      }
     }
   });
 });
